126cd96fd8ff51a52dfd9b28a550ffa9f74b6086
Parvi Chakravarti <parvichakravarti10@gmail.com>
2025-08-24 11:01:41 +0530

    Update Datasets.py

--- screens/Datasets.py
+++ screens/Datasets.py
@@ -1,19 +1,26 @@
 import streamlit as st
 import pandas as pd
+import os
 from components.navbar import navbar
 
 def show_Datasets():
     navbar("Datasets")
     st.title("Maharashtra Deforestation Data")
 
+
+    # Get project root directory (folder where app.py lives)
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
     # --- Sub Page 1 ---
     col1, col2 = st.columns([1,2])
     with col1:
         st.title("Data Analysis")
     with col2:
-        st.image(
-            r"C:\Users\parvi\OneDrive\Documents\stream--deforestation\images\mh.webp",caption="Primary Forest Loss in Maharashtra"
-        )
+        # Build correct image path
+        img_path = os.path.join(BASE_DIR, "images", "mh.webp")
+
+        # Display image
+        st.image(img_path, caption="Primary Forest Loss in Maharashtra")
 
         
 
@@ -28,4 +35,5 @@ def show_Datasets():
     st.write("Columns:", df.columns.tolist())
     st.write("State:", df.State.unique())
     st.write("Null Values:", df.isnull().sum())
+
     st.write("Month:", df.Month.unique())
